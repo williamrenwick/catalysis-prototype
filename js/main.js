c7a6859b805e2dc9c54941ce74553d1c5a878635
virtillo <virtillo@gmail.com>
2015-01-09 11:20:55 +0000

    Included Slider on landing page

--- js/main.js
+++ js/main.js
@@ -108,3 +108,134 @@ $(function(){
 	createHamburger();
 	menuObj.events();
 });
+
+
+/*
+SLIDESHOW CONTROL
+*/
+var animStop = true;
+
+$("#arrowRight").click(function(){
+	
+	if(animStop){
+		
+	animStop = false;
+
+	var currentSlide   = $(".currentSlide"),
+		nextSlide	   = currentSlide.next('.slide'),
+		selectedButton = $(".slideshowMenuItemSelected"),
+		nextButton	   = selectedButton.next('.slideshowMenuItem');
+		
+	if(nextSlide.length==0)	
+	{
+		nextSlide = $('.slide').first();
+		nextButton = $('.slideshowMenuItem').first();
+	}
+	
+	currentSlide.animate({ "left": "-100%" } );
+	nextSlide.css("left","100%")
+	
+	
+	nextSlide.animate({ "left": "0%" } ,function(){
+		animStop = true;
+	});
+	
+	currentSlide.removeClass("currentSlide");
+	nextSlide.addClass("currentSlide");
+	
+	selectedButton.removeClass("slideshowMenuItemSelected");
+	nextButton.addClass("slideshowMenuItemSelected");
+	
+	
+	}
+
+});
+
+$("#arrowLeft").click(function(){
+	
+	if(animStop){
+	
+	animStop = false;
+
+	var currentSlide   = $(".currentSlide"),
+		prevSlide	   = currentSlide.prev('.slide'),
+		selectedButton = $(".slideshowMenuItemSelected"),
+		prevButton	   = selectedButton.prev('.slideshowMenuItem');
+		
+	if(prevSlide.length==0)	
+	{
+		prevSlide = $('.slide').last();
+		prevButton = $('.slideshowMenuItem').last();
+	}
+	
+	currentSlide.animate({ "left": "100%" } );
+	prevSlide.css("left","-100%")
+	prevSlide.animate({ "left": "0%" } ,function(){
+		animStop = true;
+	});
+	
+	currentSlide.removeClass("currentSlide");
+	prevSlide.addClass("currentSlide");
+	
+	selectedButton.removeClass("slideshowMenuItemSelected");
+	prevButton.addClass("slideshowMenuItemSelected");
+	
+	}
+});
+
+var test
+
+$(".slideshowMenuItem").click(function(){
+	var currentSlideN  = $(".currentSlide").index(),
+		clickedSlideN  = $(this).index();
+	var selectedButton = $(".slideshowMenuItemSelected");
+	var currentEl	   = $(".currentSlide");
+	currentEl.removeClass("currentSlide");
+	
+	var diff = currentSlideN - clickedSlideN;
+	
+	if(diff<0){
+		diff *= -1; 
+		leftDiff = "-=" + diff*100 + "%";
+		
+		currentEl.animate({ "left": leftDiff } , 1000 , function(){
+				animStop = true;
+			});
+			
+		for(i=0;i<diff;i++)
+		{
+			currentEl = currentEl.next();
+			leftValue = (i+1)*100 + "%";
+			currentEl.css("left",leftValue)
+			currentEl.animate({ "left": leftDiff } , 1000 , function(){
+				animStop = true;
+			});
+		}
+			
+	}
+	
+	else if(diff>0){
+		leftDiff = "+=" + diff*100 + "%";
+		
+		currentEl.animate({ "left": leftDiff } , 1000 , function(){
+				animStop = true;
+			});
+			
+		for(i=0;i<diff;i++)
+		{
+			currentEl = currentEl.prev();
+			leftValue = "-" + (i+1)*100 + "%";
+			currentEl.css("left",leftValue)
+			currentEl.animate({ "left": leftDiff } , 1000 , function(){
+				animStop = true;
+			});
+		}
+			
+	}
+	
+	currentEl.addClass("currentSlide");
+	
+	selectedButton.removeClass("slideshowMenuItemSelected");
+	$(this).addClass("slideshowMenuItemSelected");
+	
+});
