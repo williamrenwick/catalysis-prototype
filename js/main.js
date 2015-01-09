b8369ce3b233467e76b79a52db31c73a7e39c555
williamrenwick <williamrenwick@Williams-iMac.local>
2015-01-09 10:39:35 +0000

    Re upload File Structure

--- js/main.js
+++ js/main.js
@@ -1,142 +1,89 @@
 $(function(){
-	var menuOpen = false;
-	var $menu = $(".menu-button");
-	var $menuwrapper = $('.menu-wrapper');
-	var body = $('body');
-	var $menuBar = $('.menu-bar-wrapper');
-	var scrollTop;
-	var wasActive;
 
-	var menuStyler = {
-		/*listPlacement: function() {
-			var $menu = $('.menu');
+	function createHamburger() {
+		var mobile = $('<div>').addClass('nav-mobile'),
+			$nav = $('.nav');
 
-			console.log($menu.height(), $menu.width())
+			//add 'nav-mobile' div to 'nav' elem
+			$nav.append(mobile);
 
-			$menu.css({
-				'margin-top': function() {
-					return - $menu.height() / 2;
-				},
-				'margin-left': function() {
-					return - $menu.width() / 2;
-				}
-			})
-		}, */
+			//add lines to mobile 
+			for (i = 0; i < 3; i++) {
+				var line = $('<div>').addClass('line'),
+				$navMob = $('.nav-mobile');
+
+				$navMob.append(line);
+			};
+	};
+
+	var menuObj = {
 		events: function() {
-			var self = this;
-			var $lines = $menu.find('.line');
-			var $text = $menu.find('.menu-text');
-			var $hb = $menu.children('.hamburger');
+			var $menubtn = $('.nav-mobile'),
+				$menuList = $('.nav-list'),
+				$fullMenu = $('#full-page-menu'),
+				$navItem = $('.item-select'),
+				menuOpen = false;
 
-			$menu.on('mouseenter', function() {
+			$navItem.on({
+				click: function() {
+					var $this = $(this),
+						type = $this.attr('class'),
+						elIndex = $this.index(),
+						$active = $('.item-select.active');
 
-				if (!menuOpen) {
-					$lines.addClass('hover');
-					$text.addClass('hover');
-				} else if (menuOpen) {
-					$hb.addClass('closehover');
+					console.log(type.indexOf('full-item'));
+					console.log(type.indexOf('nav-item'));
+
+					//remove all active classes
+					$active.removeClass('active');
+
+					//add active class to clicked elem
+					$this.addClass('active');
+
+					//add active class to related item on opposing menu
+					if (type.indexOf('full-item') == 0) {
+						var $item = $('.nav-item'),
+							itemIndex = $item.eq(elIndex);
+
+						itemIndex.addClass('active');
+					} else if (type.indexOf('nav-item') == 0) {
+						var $fullItem = $('.full-item'),
+							fullIndex = $fullItem.eq(elIndex);
+
+						fullIndex.addClass('active');
+					}
 				}
-				
 			})
-			$menu.on('mouseleave', function() {
-
-				if (!menuOpen) {
-					$lines.removeClass('hover');
-					$text.removeClass('hover');
-				} else if (menuOpen) {
-					$hb.removeClass('closehover');
-				}
-				
-			});
 			
-
-			$menu.click(function(){
+			$menubtn.on('click', function() {
 				$this = $(this);
+				if (!menuOpen) {
+					$this.addClass('close');
 
-				if(!menuOpen){
-					$lines.removeClass('hover');
-					$text.removeClass('hover');
-					$hb.removeClass('closehover');
-					$menuBar.removeClass('bgshow');
-
-					setTimeout(function() {
-						$hb.addClass('close');
-					}, 700);
-
-					$menuwrapper.fadeIn(function() {
-						//self.listPlacement();
-						$menuwrapper.children('.menu').addClass('roll-in');
-					});
-
+					$fullMenu.fadeIn();
+					//call function that creates menu list and sizes it correctly
 					menuOpen = true;
 				} else {
+					$this.removeClass('close');
 
-					$hb.removeClass('close');
-					$hb.removeClass('closehover');
-
-					if (wasActive) {
-						$menuBar.addClass('bgshow');
-					}
-
-					$menuwrapper.fadeOut(function() {
-						//self.listPlacement();
-						$menuwrapper.children('.menu').removeClass('roll-in');
-					});
-
+					$fullMenu.fadeOut();
+					//call function that removes menu list and sizes it correctly
 					menuOpen = false;
 				}
-			})
-		},
-		scrollCtrl: function() {
-			// Hide Header on on scroll down
-			var didScroll;
-			var lastScrollTop = 0;
-			var delta = 5;
-			var navbarHeight = $menuBar.outerHeight();
-
-			$(window).scroll(function(event){
-			    didScroll = true;
 			});
+		},
+		hideFullMenu: function() {
+			var $fullMenu = $('#full-page-menu'),
+				windowW = $(window).width(),
+				$menubtn = $('.nav-mobile');
 
-			setInterval(function() {
-			    if (didScroll) {
-			        hasScrolled();
-			        didScroll = false;
-			    }
-			}, 250);
-
-			function hasScrolled() {
-			    var st = $(this).scrollTop();
-			    
-			    if (st > navbarHeight*2) {
-			    	wasActive = true;
-			    	$menuBar.addClass('bgshow');
-
-			    } else {
-			    	wasActive = false;
-			    	$menuBar.removeClass('bgshow');
-			    }
-
-			    // Make sure they scroll more than delta
-			    if(Math.abs(lastScrollTop - st) <= delta)
-			        return;
-			    
-			    // If they scrolled down and are past the navbar, add class .nav-up.
-			    // This is necessary so you never see what is "behind" the navbar.
-			    if (st > lastScrollTop && st > navbarHeight){
-			        // Scroll Down
-			        $menuBar.removeClass('nav-down').addClass('nav-up');
-			    } else {
-			        // Scroll Up
-			        if(st + $(window).height() < $(document).height()) {
-			            $menuBar.removeClass('nav-up').addClass('nav-down');
-			        }
-			    }
-			    
-			    lastScrollTop = st;
+			if (windowW > 768) {
+				$fullMenu.css({'display': 'none'});
+				$menubtn.removeClass('close');
 			}
 		}
-	}
+	};
+
 
 	function debounce(func, wait, immediate) {
 		var timeout;
@@ -153,16 +100,11 @@ $(function(){
 		};
 	};
 	var resizeFn = debounce(function() {
-		//menuStyler.listPlacement();
-	}, 0);
+		menuObj.hideFullMenu();
+	}, 100);
 
 	window.addEventListener('resize', resizeFn);
 
-	function initialise() {
-		menuStyler.events();
-		menuStyler.scrollCtrl()
-	}
-
-
-	initialise();
+	createHamburger();
+	menuObj.events();
 });
