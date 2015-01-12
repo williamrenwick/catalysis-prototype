f2a1411216d06e1e6eab55970e64254b895d458b
williamrenwick <williamrenwick@williams-mbp.home>
2015-01-12 14:57:44 +0000

    More Media Queries
    Added more media queries, max width on main wrapper etc. etc.

--- js/main.js
+++ js/main.js
@@ -1,5 +1,51 @@
 $(function(){
 
+	//Show and Hide Nav on Scroll
+
+	// Hide Header on on scroll down
+	var didScroll;
+	var lastScrollTop = 0;
+	var delta = 5;
+	var navbarHeight = $('.nav').outerHeight();
+
+	$(window).scroll(function(event){
+	    didScroll = true;
+	});
+
+	setInterval(function() {
+	    if (didScroll) {
+	        hasScrolled();
+	        didScroll = false;
+	    }
+	}, 250);
+
+	function hasScrolled() {
+	    var st = $(this).scrollTop(),
+	    	$nav = $('.nav'),
+	    	bodyBGColor = $('body').css('background-color');
+	    
+	    // Make sure they scroll more than delta
+	    if(Math.abs(lastScrollTop - st) <= delta)
+	        return;
+	    
+	    // If they scrolled down and are past the navbar, add class .nav-up.
+	    // This is necessary so you never see what is "behind" the navbar.
+	    if (st > lastScrollTop && st > navbarHeight){
+	        // Scroll Down
+	        $nav.removeClass('nav-down').addClass('nav-up');
+	        $nav.css({
+	        	'background-color': bodyBGColor
+	        });
+	    } else {
+	        // Scroll Up
+	        if(st + $(window).height() < $(document).height()) {
+	            $nav.removeClass('nav-up').addClass('nav-down');
+	        }
+	    }
+	    
+	    lastScrollTop = st;
+	}
+
 	function createHamburger() {
 		var mobile = $('<div>').addClass('nav-mobile'),
 			$nav = $('.nav');
